020996ba07b0a67f8c13d473b6b177f7e7239428
Chris Griffith <chris@cdgriffith.com>
2018-09-22 00:02:57 -0500

    shows cover, can click and view video

--- routes/index.js
+++ routes/index.js
@@ -4,63 +4,56 @@ const {generateToken} = require('../lib/auth')
 const {getProvider} = require('../lib/storage')
 const request = require('request')
 const Asset = require('../lib/models/asset')()
+const {decrypt} = require('../lib/encryption')
+const {log} = require('winston')
 
 const provider = getProvider()
 const storage = new provider()
 
 
 router.get('/', async function (req, res) {
-  res.cookie('token', await generateToken('test'), {maxAge: 24 * 60 * 60 * 1000, httpOnly: true, signed: true}).render('login')
+  res.cookie('token', await generateToken('test'), {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    signed: true
+  }).render('login')
 })
 
-router.post('/', async function (req, res) {
-  res.redirect('/video')
-})
 
-router.get('/asset/:id/*', async function (req, res) {
+router.get('/asset/mpeg_dash/:id/*', async function (req, res) {
   let contentId = req.params.id
   let filePath = req.params[0]
 
-  request(await storage.streamUrl(`mpeg_dash/${contentId}/${filePath}`, false), {headers: {Authorization: storage.authToken}}).pipe(res)
+  request(await storage.streamUrl(`mpeg_dash/${contentId}/${filePath}`, false),
+      {headers: {Authorization: storage.authToken}}).pipe(res)
 })
+
+
+router.get('/asset/covers/:id', async function (req, res) {
+  let record = await Asset.findOne({media_id: req.params.id})
+  let decrypted
+  try {
+    let data = await storage.downloadFile(`covers/${req.params.id}.jpg`)
+    decrypted = await decrypt({encrypted: data, ...record.cover})
+  } catch (err){
+    log('error', err.message)
+    throw err
+  }
+  res.contentType('image/jpeg')
+  res.end(decrypted, 'binary')
+  })
 
 
 router.get('/folder/:folder', async function (req, res) {
-  let bucketId, files
-  try {
-    bucketId = await bb._findBucketId('griff-test')
-    files = await bb.listFiles(bucketId)
-  } catch (err){
-    console.log(err)
-    throw err
+  let records = await Asset.find({media_type: req.params.folder.trim()})
+  let files = []
+  for (let file of records){
+    files.push({cover: `/asset/covers/${file.media_id}`, media_id: file.media_id})
   }
-  let folderNames = await getFolders(bb, bucketId, req.params.folder)
-  let folders = []
 
-  folderNames.forEach(folder => {
-    folders.push({name: folder, cover: bb.downloadFileNameUrl(process.env.STORAGE_BUCKET_NAME, `${req.params.folder}/${folder}/cover.jpg`)})
-  })
-
-  res.render('folder_viewer', {base: req.params.folder, folders: folders})
+  res.render('folder_viewer', {base: req.params.folder, folders: files})
 })
 
-router.get('/video', async function (req, res) {
-  let bucketId, files
-  try {
-    bucketId = await bb._findBucketId('griff-test')
-    files = await bb.listFiles(bucketId)
-  } catch (err){
-    console.log(err)
-    throw err
-  }
-  let fileNames = []
-
-  files.forEach(x => {
-    fileNames.push({fileName: x.fileName, fileId: x.fileId})
-  })
-
-  res.render('index', {files: fileNames})
-})
 
 router.get('/video/:id', async function (req, res, next) {
   let record = await Asset.findOne({media_id: req.params.id})
