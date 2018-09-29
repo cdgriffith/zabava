9aa3d3c8b93013d42e89d2e97b2b7211d0572e6e
Chris Griffith <chris@cdgriffith.com>
2018-09-28 20:17:04 -0500

    new upload, auth redirect

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
+
+  if (err.status === 401){
+    res.clearCookie('token')
+    return res.redirect("/")
+  }
+
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
